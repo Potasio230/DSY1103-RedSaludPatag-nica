dd6823ff8352edabcc6035892bb265bcd67f4670
Francisco Molina <fmolina.inf@gmail.com>
2026-04-27 12:17:34 -0400

    Dependencias

---
+++ src/test/java/com/duoc/RedSalud/RedSaludApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.RedSalud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedSaludApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
